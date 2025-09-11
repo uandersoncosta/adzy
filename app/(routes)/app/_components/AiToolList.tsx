9f6fc5550a0af2182c5215f218cdb4939d2c20f2
Uanderson Costa <uandersonferreiracosta@gmail.com>
2025-09-11 19:29:45 -0300

    chore: terminado os components do dashboard

--- app/(routes)/app/_components/AiToolList.tsx
+++ app/(routes)/app/_components/AiToolList.tsx
@@ -1,37 +1,44 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import React from 'react'
 
 const aiTools = [
   {
-    name: 'Ai Product Image',
-    desc: 'Generate High-quality, professional product images instantly with AI',
-    bannerImage: '/product-image.png',
+    name: "Imagem de Produto com IA",
+    desc: "Gere imagens de produto profissionais e de alta qualidade instantaneamente com IA",
+    bannerImage: "/product-image.png",
+    path: "/"
   },
   {
-    name: 'Ai Product Video',
-    desc: 'Create engaging product showcase video using AI',
-    bannerImage: '/product-avatar.png',
+    name: "Vídeo de Produto com IA",
+    desc: "Crie vídeos envolventes de apresentação de produto usando IA",
+    bannerImage: "/product-avatar.png",
+    path: "/"
   },
   {
-    name: 'Ai Product With Avatar',
-    desc: 'Bring your product to life with AI avatars',
-    bannerImage: '/product-video.png',
+    name: "Produto com Avatar IA",
+    desc: "Dê vida ao seu produto com avatares gerados por IA",
+    bannerImage: "/product-video.png",
+    path: "/"
   }
+
 ]
 
 function AiToolList() {
   return (
     <div>
       <h2 className='font-bold text-2xl mb-2 '>Creative Tools</h2>
-      <div>{aiTools.map((tool, index) => (
-        <div key={index}>
-          <div className=''>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>{aiTools.map((tool, index) => (
+        <div key={index} className='flex items-center justify-between p-7 bg-zinc-700 rounded-2xl'>
+          <div>
             <h2 className='font-bold text-2xl'>{tool.name}</h2>
             <p className='opacity-60 mt-2'>{tool.desc}</p>
+            <Button className='mt-4'>Criar Agora</Button>
           </div>
-          <Image src={tool.bannerImage} alt={tool.name} width={300} height={300} className='w-[200px]'/>
+          <Image src={tool.bannerImage} alt={tool.name} width={300} height={300} className='w-[200px]' />
         </div>
-      ))}</div>
+      ))}
+      </div>
     </div>
   )
 }
