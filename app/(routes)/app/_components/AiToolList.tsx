d8da4b8a2b1d2ff01b5ca1bb2e29e41a1e935f28
Uanderson Costa <uandersonferreiracosta@gmail.com>
2025-09-11 20:22:02 -0300

    chore: atualizando o appsidebar com useAuthContext verificando o user

--- app/(routes)/app/_components/AiToolList.tsx
+++ app/(routes)/app/_components/AiToolList.tsx
@@ -1,46 +1,55 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import React from "react";
 
 const aiTools = [
   {
     name: "Imagem de Produto com IA",
     desc: "Gere imagens de produto profissionais e de alta qualidade instantaneamente com IA",
     bannerImage: "/product-image.png",
-    path: "/"
+    path: "/",
   },
   {
     name: "Vídeo de Produto com IA",
     desc: "Crie vídeos envolventes de apresentação de produto usando IA",
     bannerImage: "/product-avatar.png",
-    path: "/"
+    path: "/",
   },
   {
     name: "Produto com Avatar IA",
     desc: "Dê vida ao seu produto com avatares gerados por IA",
     bannerImage: "/product-video.png",
-    path: "/"
-  }
-
-]
+    path: "/",
+  },
+];
 
 function AiToolList() {
   return (
     <div>
-      <h2 className='font-bold text-2xl mb-2 '>Creative Tools</h2>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>{aiTools.map((tool, index) => (
-        <div key={index} className='flex items-center justify-between p-7 bg-zinc-700 rounded-2xl'>
-          <div>
-            <h2 className='font-bold text-2xl'>{tool.name}</h2>
-            <p className='opacity-60 mt-2'>{tool.desc}</p>
-            <Button className='mt-4'>Criar Agora</Button>
+      <h2 className="font-bold text-2xl mb-2 ">Creative Tools</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+        {aiTools.map((tool, index) => (
+          <div
+            key={index}
+            className="flex items-center justify-between p-7 bg-zinc-700 rounded-2xl"
+          >
+            <div>
+              <h2 className="font-bold text-2xl">{tool.name}</h2>
+              <p className="opacity-60 mt-2">{tool.desc}</p>
+              <Button className="mt-4">Criar Agora</Button>
+            </div>
+            <Image
+              src={tool.bannerImage}
+              alt={tool.name}
+              width={300}
+              height={300}
+              className="w-[200px]"
+            />
           </div>
-          <Image src={tool.bannerImage} alt={tool.name} width={300} height={300} className='w-[200px]' />
-        </div>
-      ))}
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default AiToolList
+export default AiToolList;
--- app/_components/AppSidebar.tsx
+++ app/_components/AppSidebar.tsx
@@ -1,85 +1,118 @@
-import React from 'react'
+import React from "react";
 import {
-    Sidebar,
-    SidebarContent,
-    SidebarFooter,
-    SidebarGroup,
-    SidebarGroupContent,
-    SidebarGroupLabel,
-    SidebarHeader,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
-} from "@/components/ui/sidebar"
-import { Calendar, Home, Inbox, Megaphone, Search, Settings, Wallet2 } from "lucide-react"
-import Image from 'next/image'
-import { useParams, usePathname } from 'next/navigation'
-import { Button } from '@/components/ui/button'
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import {
+  Calendar,
+  Home,
+  Inbox,
+  Megaphone,
+  Search,
+  Settings,
+  Wallet2,
+} from "lucide-react";
+import Image from "next/image";
+import { useParams, usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useAuthContext } from "../provider";
+import ProfileAvatar from "./ProfileAvatar";
 
 const items = [
-    {
-        title: "Home",
-        url: "/app",
-        icon: Home,
-    },
-    {
-        title: "Creative Tools",
-        url: "/creative-tools",
-        icon: Inbox,
-    },
-    {
-        title: "My Ads",
-        url: "#",
-        icon: Megaphone,
-    },
-    {
-        title: "Upgrade",
-        url: "#",
-        icon: Wallet2,
-    },
-    {
-        title: "Profile",
-        url: "#",
-        icon: Settings,
-    },
-]
+  {
+    title: "Home",
+    url: "/app",
+    icon: Home,
+  },
+  {
+    title: "Creative Tools",
+    url: "/creative-tools",
+    icon: Inbox,
+  },
+  {
+    title: "My Ads",
+    url: "#",
+    icon: Megaphone,
+  },
+  {
+    title: "Upgrade",
+    url: "#",
+    icon: Wallet2,
+  },
+  {
+    title: "Profile",
+    url: "#",
+    icon: Settings,
+  },
+];
 
 export function AppSidebar() {
-    const path = usePathname();
-    return (
-        <Sidebar>
-            <SidebarHeader>
-                <div className='p-4'>
-                    <Image src={'./logo.svg'} alt='logo' width={100} height={100}
-                        className='w-full h-full' />
-                    <h2 className='text-sm text-gray-400 text-center'>Build Awesome</h2>
-                </div>
-            </SidebarHeader>
-            <SidebarContent>
-                <SidebarGroup>
+  const path = usePathname();
+  const { user } = useAuthContext();
 
-                    <SidebarGroupContent>
-                        <SidebarMenu className='mt-5'>
-                            {items.map((item, index) => (
-                                // <SidebarMenuItem key={item.title} className='p-2'>
-                                //     <SidebarMenuButton asChild className=''>
-                                <a href={item.url} key={index} className={`p-2 text-lg flex gap-2 items-center
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <div className="p-4">
+          <Image
+            src={"./logo.svg"}
+            alt="logo"
+            width={100}
+            height={100}
+            className="w-full h-full"
+          />
+          <h2 className="text-sm text-gray-400 text-center">Build Awesome</h2>
+        </div>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu className="mt-5">
+              {items.map((item, index) => (
+                // <SidebarMenuItem key={item.title} className='p-2'>
+                //     <SidebarMenuButton asChild className=''>
+                <a
+                  href={item.url}
+                  key={index}
+                  className={`p-2 text-lg flex gap-2 items-center
                                  hover:bg-gray-100 dark:hover:bg-zinc-800 rounded-lg
-                                 ${path == item.url && 'bg-gray-100 dark:bg-zinc-800'}`}>
-                                    <item.icon className='h-5 w-5' />
-                                    <span>{item.title}</span>
-                                </a>
-                                //     </SidebarMenuButton>
-                                // </SidebarMenuItem>
-                            ))}
-                        </SidebarMenu>
-                    </SidebarGroupContent>
-                </SidebarGroup>
-            </SidebarContent>
-            <SidebarFooter>
-                <Button>Sign in</Button>
-                <h2 className='p-2 text-gray-400 text-sm'>Copyright @uandersoncosta</h2>
-            </SidebarFooter>
-        </Sidebar>
-    )
+                                 ${
+                                   path == item.url &&
+                                   "bg-gray-100 dark:bg-zinc-800"
+                                 }`}
+                >
+                  <item.icon className="h-5 w-5" />
+                  <span>{item.title}</span>
+                </a>
+                //     </SidebarMenuButton>
+                // </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        {!user ? (
+          <Link href={"/login"} className="w-full">
+            <Button className="w-full">Sign in</Button>
+          </Link>
+        ) : (
+          <div className="flex justify-between items-center p-2 px-4 bg-zinc-800 rounded-lg">
+            <h2>Profile</h2>
+            <ProfileAvatar />
+          </div>
+        )}
+        <h2 className="p-2 text-gray-400 text-sm">Copyright @uandersoncosta</h2>
+      </SidebarFooter>
+    </Sidebar>
+  );
 }
